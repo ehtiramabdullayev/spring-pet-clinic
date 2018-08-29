2ce03f8aa49423a08e84cf852a4ff268e986fa1e
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-08-29 16:12:45 +0200

    Adding in Index Page and Controller. Closes #8

--- pet-clinic-web/src/main/java/com/springframework/springpetclinic/SpringPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/SpringPetClinicApplication.java
@@ -1,5 +1,12 @@
+package com.springframework.springpetclinic;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author Ehtiram_Abdullayev on 8/29/2018
+ * @project spring-pet-clinic
+ */
 
 @SpringBootApplication
 public class SpringPetClinicApplication {
---
+++ pet-clinic-web/src/main/java/com/springframework/springpetclinic/controllers/IndexController.java
@@ -0,0 +1,21 @@
+package com.springframework.springpetclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Ehtiram_Abdullayev on 8/29/2018
+ * @project spring-pet-clinic
+ */
+
+@Controller
+public class IndexController {
+
+    @RequestMapping({"","/","index","index.html"})
+    public String index(){
+        return "index";
+    }
+
+
+
+}
