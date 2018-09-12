6fd986a078f2b74b87f9eb0e54f7f8b720a54203
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-09-12 14:35:15 +0200

    pushing the changed files to git

--- pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/AbstractMapService.java
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/map/AbstractMapService.java
@@ -10,7 +10,7 @@ import java.util.*;
 public abstract class AbstractMapService<T extends BaseEntity, ID extends Long> {
     protected Map<Long, T> map = new HashMap<>();
 
-    Set<T> findAll() {
+    Set<T> findAll() { 
         return new HashSet<>(map.values());
     }
 
