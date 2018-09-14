bc7b4ce4808722c7482c0f79cdef6a54794f005a
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-09-14 11:20:34 +0200

    Added PetTypeService Closes #29

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/services/PetTypeService.java
@@ -0,0 +1,10 @@
+package com.springframework.springpetclinic.services;
+
+import com.springframework.springpetclinic.model.PetType;
+
+/**
+ * @author Ehtiram_Abdullayev on 9/14/2018
+ * @project spring-pet-clinic
+ */
+public interface PetTypeService extends CrudService<PetType,Long> {
+}
