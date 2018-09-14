43a31f95205b190a76f881eb45abeb66f7fbf670
Ehtiram Abdullayev <ehtiramabdullayev@gmail.com>
2018-09-14 10:44:55 +0200

    Created Visit and Owner has Set of Pets now.  Closes #26

---
+++ pet-clinic-data/src/main/java/com/springframework/springpetclinic/model/Visit.java
@@ -0,0 +1,39 @@
+package com.springframework.springpetclinic.model;
+
+import java.time.LocalDate;
+
+/**
+ * @author Ehtiram_Abdullayev on 9/14/2018
+ * @project spring-pet-clinic
+ */
+
+public class Visit extends BaseEntity {
+    private LocalDate date;
+    private String description;
+    private Pet pet;
+
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Pet getPet() {
+        return pet;
+    }
+
+    public void setPet(Pet pet) {
+        this.pet = pet;
+    }
+}
